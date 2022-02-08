9c347d96928a5456c4e7f52b4411c31aa0f091be
Adirelle <adirelle@gmail.com>
2022-02-08 12:36:56 +0100

    Reworked Waila plugin.

--- src/main/kotlin/dev/adirelle/adicrate/bridge/waila/CrateProvider.kt
+++ src/main/kotlin/dev/adirelle/adicrate/bridge/waila/CrateProvider.kt
@@ -6,19 +6,24 @@ import dev.adirelle.adicrate.AdiCrate
 import dev.adirelle.adicrate.block.entity.CrateBlockEntity
 import dev.adirelle.adicrate.block.entity.internal.CrateStorage
 import dev.adirelle.adicrate.utils.extensions.set
+import dev.adirelle.adicrate.utils.logger
 import mcp.mobius.waila.api.*
 import mcp.mobius.waila.api.component.ItemComponent
 import mcp.mobius.waila.api.component.PairComponent
 import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant
 import net.minecraft.client.MinecraftClient
+import net.minecraft.client.item.TooltipContext
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.LiteralText
 import net.minecraft.text.TranslatableText
 import net.minecraft.util.math.Direction
 import net.minecraft.world.World
+import java.util.*
 
 object CrateProvider : IWailaPlugin, IBlockComponentProvider, IServerDataProvider<CrateBlockEntity> {
+
+    private val LOGGER by logger
 
     val showContent = AdiCrate.id("show_content")
     val showController = AdiCrate.id("show_controller")
@@ -39,9 +44,11 @@ object CrateProvider : IWailaPlugin, IBlockComponentProvider, IServerDataProvide
         registrar.addSyncedConfig(showContent, true)
         registrar.addSyncedConfig(showController, true)
         registrar.addSyncedConfig(showInteractionHints, true)
-        registrar.addComponent(this, TooltipPosition.BODY, CrateBlockEntity::class.java)
-        registrar.addComponent(this, TooltipPosition.TAIL, CrateBlockEntity::class.java)
-        registrar.addBlockData(this, CrateBlockEntity::class.java)
+        val clazz = CrateBlockEntity::class.java
+        registrar.addIcon(this, clazz)
+        registrar.addComponent(this, TooltipPosition.BODY, clazz)
+        registrar.addComponent(this, TooltipPosition.TAIL, clazz)
+        registrar.addBlockData(this, clazz)
     }
 
     override fun appendServerData(
@@ -50,45 +57,56 @@ object CrateProvider : IWailaPlugin, IBlockComponentProvider, IServerDataProvide
         world: World,
         blockEntity: CrateBlockEntity
     ) {
-        data[DATA_KEY] = { nbt ->
-            with(blockEntity.storage) {
-                nbt["resource"] = resource
-                nbt["amount"] = amount
-                nbt["capacity"] = realCapacity
-                nbt["jammed"] = isJammed()
+        data[DATA_KEY] = ServerData.of(blockEntity)
+            .writeNbt(NbtCompound())
+    }
+
+    private inline fun <R> IBlockAccessor.withServerData(crossinline block: (ServerData) -> R): Optional<R> =
+        Optional
+            .of(serverData.getCompound(DATA_KEY))
+            .filter { !it.isEmpty }
+            .map(ServerData.Companion::fromNbt)
+            .map { block(it) }
+
+    override fun getIcon(accessor: IBlockAccessor, config: IPluginConfig): ItemComponent? {
+        LOGGER.info("getIcon, data={}", accessor.serverData.asString())
+        return accessor
+            .withServerData { data ->
+                if (true || data.isFacing(accessor.side))
+                    ItemComponent(data.resource.toStack())
+                else
+                    null
             }
-            nbt["facing"] = blockEntity.facing.toString()
-            blockEntity.networkInfo?.let {
-                nbt["networkInfo"] = it.name
-            }
-        }
+            .orElse(null)
+
     }
 
     override fun appendBody(tooltip: ITooltip, accessor: IBlockAccessor, config: IPluginConfig) {
-        val nbt = accessor.serverData.getCompound(DATA_KEY)
+        accessor.withServerData { data ->
 
-        if (config.showContent) {
-            val resource = ItemVariant.fromNbt(nbt.getCompound("resource"))
-            tooltip.addLine()
-                .with(ItemComponent(resource.toStack(1)))
-                .with(CrateStorage.contentText(nbt.getLong("amount"), nbt.getLong("capacity")))
-        }
+            if (config.showContent) {
+                val stack = data.resource.toStack(data.amount.toInt())
 
-        if (config.showController) {
-            val networkInfo = nbt.getString("networkInfo")
-            if (!networkInfo.isEmpty()) {
+                if (!stack.isEmpty) {
+                    stack.getTooltip(accessor.player, TooltipContext.Default.NORMAL)
+                        .forEach(tooltip::addLine)
+                }
+
+                tooltip.addLine()
+                    .with(CrateStorage.contentText(data.amount, data.capacity))
+            }
+
+            if (config.showController && data.networkInfo.isNotEmpty()) {
                 tooltip.addLine()
                     .with(
                         PairComponent(
                             TranslatableText("tooltip.adicrate.crate.network_info"),
-                            LiteralText(networkInfo)
+                            LiteralText(data.networkInfo)
                         )
                     )
             }
-        }
 
-        if (config.showContent) {
-            if (nbt.getBoolean("jammed")) {
+            if (config.showContent && data.isJammed) {
                 tooltip.addLine(TranslatableText("tooltip.adicrate.crate.jammed"))
             }
         }
@@ -96,8 +114,6 @@ object CrateProvider : IWailaPlugin, IBlockComponentProvider, IServerDataProvide
 
     override fun appendTail(tooltip: ITooltip, accessor: IBlockAccessor, config: IPluginConfig) {
         if (!config.showInteractionHints) return
-
-        val facing = Direction.valueOf(accessor.serverData.getCompound(DATA_KEY).getString("facing"))
 
         fun addHint(id: String, vararg args: Any) {
             tooltip.addLine(TranslatableText("tooltip.adicrate.crate.interaction_hints.$id", *args))
@@ -108,7 +124,7 @@ object CrateProvider : IWailaPlugin, IBlockComponentProvider, IServerDataProvide
             val attackKey = keyAttack.boundKeyLocalizedText
             val sneakKey = keySneak.boundKeyLocalizedText
 
-            if (accessor.side == facing) {
+            if (accessor.withServerData { it.isFacing(accessor.side) }.orElse(false)) {
                 addHint("pull_item", attackKey)
                 addHint("pull_one_item", sneakKey, attackKey)
                 addHint("push_item", useKey)
@@ -116,6 +132,53 @@ object CrateProvider : IWailaPlugin, IBlockComponentProvider, IServerDataProvide
             } else {
                 addHint("open_gui", useKey)
             }
+        }
+    }
+
+    private data class ServerData(
+        val resource: ItemVariant,
+        val amount: Long,
+        val capacity: Long,
+        val isJammed: Boolean,
+        val facing: Direction,
+        var networkInfo: String
+    ) {
+
+        companion object {
+
+            fun of(blockEntity: CrateBlockEntity) =
+                ServerData(
+                    blockEntity.storage.resource,
+                    blockEntity.storage.amount,
+                    blockEntity.storage.realCapacity,
+                    blockEntity.storage.isJammed(),
+                    blockEntity.facing,
+                    blockEntity.networkInfo?.name ?: ""
+                )
+
+            fun fromNbt(nbt: NbtCompound) =
+                with(nbt) {
+                    ServerData(
+                        ItemVariant.fromNbt(getCompound("resource")),
+                        getLong("amount"),
+                        getLong("capacity"),
+                        getBoolean("jammed"),
+                        Direction.fromHorizontal(getInt("facing")),
+                        getString("networkInfo")
+                    )
+                }
+        }
+
+        fun isFacing(direction: Direction): Boolean =
+            direction == facing
+
+        fun writeNbt(nbt: NbtCompound) = nbt.also {
+            it["resource"] = resource
+            it["amount"] = amount
+            it["capacity"] = capacity
+            it["jammed"] = isJammed
+            it["facing"] = facing.horizontal
+            it["networkInfo"] = networkInfo
         }
     }
 }
